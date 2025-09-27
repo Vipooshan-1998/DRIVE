492cad158acd40cfcac3204a7c69f2d635c0f331
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-09-27 09:17:06 +0530

    Update DADALoader.py

--- src/DADALoader.py
+++ src/DADALoader.py
@@ -45,22 +45,23 @@ class DADALoader(Dataset):
     #     return data_list
 
     def get_data_list(self):
-    # video path
-    rgb_path = os.path.join(self.root_path, self.phase, 'rgb_videos', self.phase)
-    assert os.path.exists(rgb_path), "Path does not exist! %s" % (rgb_path)
-
-    data_list = []
-    # loop for each type of accident (e.g., 'negative', 'positive', etc.)
-    for accident in sorted(os.listdir(rgb_path)):
-        if self.cls_task and accident not in self.accident_classes:
-            # for accident classification task, ignore categories with too few videos
-            continue
-        accident_rgb_path = os.path.join(rgb_path, accident)
-        for vid in sorted(os.listdir(accident_rgb_path)):
-            data_list.append(accident + '/' + os.path.splitext(vid)[0])  # strip extension
-    print("--------------------data_list------------------")
-    print(data_list)
-    return data_list
+        # video path
+        rgb_path = os.path.join(self.root_path, self.phase, 'rgb_videos', self.phase)
+        assert os.path.exists(rgb_path), "Path does not exist! %s" % (rgb_path)
+    
+        data_list = []
+        # loop for each type of accident (e.g., 'negative', 'positive', etc.)
+        for accident in sorted(os.listdir(rgb_path)):
+            if self.cls_task and accident not in self.accident_classes:
+                # for accident classification task, ignore categories with too few videos
+                continue
+            accident_rgb_path = os.path.join(rgb_path, accident)
+            for vid in sorted(os.listdir(accident_rgb_path)):
+                data_list.append(accident + '/' + os.path.splitext(vid)[0])  # strip extension
+                
+        print("--------------------data_list------------------")
+        print(data_list)
+        return data_list
 
 
 
